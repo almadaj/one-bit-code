4572f25cae318a9dff002dbf49cdfd365d6f233d
Juliano Almada <almadaj@edu.unifor.br>
2023-07-01 20:01:02 -0300

    começando typescript

---
+++ 8 - TypeScript/1 - Instalando TypeScript/index.ts
@@ -0,0 +1,44 @@
+const setPilot = async (
+  newPilot: string,
+  spaceship: {
+    name?: string;
+    pilot: string;
+    speed?: number;
+    inMission?: boolean;
+  }
+) => {
+  spaceship.pilot = newPilot;
+};
+
+const accelerate = async (
+  targetSpeed: number,
+  spaceship: { name?: string; pilot?: string; speed: any; inMission?: boolean }
+) => {
+  spaceship.speed = targetSpeed;
+};
+
+const sendToMission = async (spaceship: {
+  name?: string;
+  pilot?: string;
+  speed?: number;
+  inMission: any;
+}) => {
+  spaceship.inMission = true;
+};
+
+const spaceship = {
+  name: "",
+  pilot: "",
+  speed: 0,
+  inMission: false,
+};
+
+const pilot = "Han Solo";
+
+spaceship.name = "Millennium Falcon";
+
+setPilot(pilot, spaceship);
+accelerate(50, spaceship);
+sendToMission(spaceship);
+
+console.log(spaceship);
