e8883c2076127e0b3597e1f916b940ffa0fbcfb9
Juliano Almada <almadaj@edu.unifor.br>
2023-09-09 17:13:24 -0300

    exercícios de react

---
+++ 12 - React/3 - Vite/exercicio-1/src/App.jsx
@@ -0,0 +1,33 @@
+export default function App() {
+  return (
+    <div>
+      <img src="public/react.png" alt="logo-react" />
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuário web e nativas</p>
+      <button>Aprenda React</button>
+      <button>Referência da API</button>
+      <hr />
+      <h2>Crie Interfaces de usuário de componentes</h2>
+      <p>
+        React permite que você construa interfaces de usuário a partir de
+        pedaços individuais chamados componentes
+      </p>
+      <hr />
+      <h2>Escreva componentes com código e marcação</h2>
+      <p>
+        Componentes são funções JavaScript. A sintaxe de marcação é chamada de
+        JSX. É uma extensão da sintaxe de JavaScript popularizada como React
+      </p>
+      <hr />
+      <p>Próximos passos:</p>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Reutilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </div>
+  );
+}
---
+++ 12 - React/3 - Vite/primeiro-app/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Olá, Mundo!</h1>
+      <p>Este é o meu primeiro projeto usando React e Vite!</p>
+      <p>Próximos passos:</p>
+      <ul>
+        <li>Praticar com exercícios</li>
+        <li>Aprender a usar dados dinâmicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar componentes</li>
+      </ul>
+    </div>
+  );
+}
