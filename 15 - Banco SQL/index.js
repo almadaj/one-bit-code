fd959db372ec238fb1e9f1a56b474b88800e5626
Juliano Almada <almadaj@edu.unifor.br>
2023-12-04 14:08:23 -0300

    criação de CRUD dentro do banco de dados

--- 15 - Banco SQL/index.js
+++ 15 - Banco SQL/index.js
@@ -1,9 +1,30 @@
+const { where } = require("sequelize");
+
 (async () => {
   const Planet = require("./models/Planet");
-
+  CREATE;
   const newPlanet = await Planet.create({
     name: "Vênus",
     position: 5,
   });
-  console.log(newPlanet);
+
+  //READ
+  // const seePlanets = await Planet.findAll({
+  //   where: {
+  //     name: "Terra",
+  //   },
+  // });
+
+  //UPDATE
+  // const updatePlanets = await Planet.findByPk(2);
+  // updatePlanets.name = "Saturno";
+
+  // await updatePlanets.save();
+
+  //DELETE
+  // const deletePlanets = await Planet.findByPk(2);
+
+  // await deletePlanets.destroy();
+
+  // console.log(deletePlanets);
 })();
